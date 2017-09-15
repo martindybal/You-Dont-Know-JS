456bf2f82990bb8e98be9dd384ce4d0c92baec42
Martin Dybal <martin@dybal.it>
2017-09-15 17:12:33 +0200

    Find project folder for generate result to right folder

---
+++ _generator/BookGenerator/BookGenerator.App/DirectoryInfoExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace BookGenerator.App
+{
+    public static class DirectoryInfoExtensions
+    {
+        public static DirectoryInfo ClosestParent(this DirectoryInfo folder, string parentFolderName)
+        {
+            var closestFolder = folder.ClosestParentOrDefault(parentFolderName); ;
+            if (closestFolder == null)
+            {
+                throw new DirectoryNotFoundException($"Directory {parentFolderName} wasn't found in {folder.FullName}");
+            }
+            return closestFolder;
+        }
+
+        public static DirectoryInfo ClosestParentOrDefault(this DirectoryInfo folder, string parentFolderName)
+        {
+            var parentFolder = folder.Parent;
+            if (parentFolder == null)
+            {
+                return null;
+            }
+            if (parentFolder.Name == parentFolderName)
+            {
+                return parentFolder;
+            }
+            return parentFolder.ClosestParent(parentFolderName);
+        }
+    }
+}
